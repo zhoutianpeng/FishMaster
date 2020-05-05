e803b3b890c317305f2825d52c2bbe17bd102c48
zhoutianpeng <zhou17806234528@163.com>
2020-05-05 19:35:26 +0800

    first commit

---
+++ Assets/scripts/Effect/Ef_WaterWave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_WaterWave : MonoBehaviour
+{
+    public Texture[] textures;
+    private Material material;
+    private int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        InvokeRepeating("changeTexture", 0, 0.1f);
+    }
+
+    void changeTexture(){
+        material.mainTexture = textures[index];
+        index = (index + 1 ) % textures.Length;
+    }
+}
